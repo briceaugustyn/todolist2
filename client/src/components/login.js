1de1fbb36b7b132a6d36a54817cc20c01cc04b73
Christian Gingrich <xian777@gmail.com>
2018-05-21 16:05:03 -0700

    added logic to app.js in client to have routes, also created template files for todolist and login components and set up routes to those components

---
+++ client/src/components/login.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+    render(){
+        return(
+            <div>
+             Login Component activated
+            </div>
+        )
+    }
+}
+export default Login;
---
+++ client/src/components/todolist.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Todolist extends Component {
+    render(){
+        return(
+            <div>
+                To do List Component
+            </div>
+        )
+    }
+}
+export default Todolist;
