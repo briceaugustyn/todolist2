cb4944817070d1164e812144947f051b2d797842
Brice Augustyn <baugustyn1000@gmail.com>
2018-05-21 19:27:28 -0700

    save version with todocode

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './components/index.css';
@@ -6,3 +7,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+
+
+// var destination = document.querySelector("#container");
+//ReactDOM.render(<div><p>Hello World Brice 2</p></div>)
