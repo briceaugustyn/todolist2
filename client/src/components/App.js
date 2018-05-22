6b033bcc819380052b0581a82ff0a05c414704bf
Christian Gingrich <xian777@gmail.com>
2018-05-21 19:27:37 -0700

    took nonsense logic out of todolist.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -41,6 +41,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1> To Do List: Get Your Shit Done! </h1>
         <nav className="navbar navbar-light">
           <ul className="nav navbar-nav">
             <li><Link to="/">Home</Link></li>
