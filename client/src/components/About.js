bc24cd3e6b7d33d10e3e5e56d1e0aa1d6eb14277
lpaul <larryf.paul@gmail.com>
2018-05-22 22:22:15 -0700

    Added About component and Dad Jokes API.

---
+++ client/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <div className="row top50">
+                <div className="col s12 m6">
+                    <h3>Blacklist ToDo List</h3>
+                    <p>
+                    This is a ToDo List application developed by the Blacklist Team for Project 2. 
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
