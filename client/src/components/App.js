84b25eb3806e19bf83eabdf343f50c0141e5ec25
Brice Augustyn <baugustyn1000@gmail.com>
2018-05-23 21:20:35 -0700

    pulled

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import logo from './logo.svg';
 import './App.css';
 
@@ -14,22 +13,34 @@ import Login from './login';
 //     <p>Hello World Brice 2</p>
 //   </div>
 // );
-=======
-import React,  {Component} from 'react';
-import { Route } from 'react-router-dom';
-import Header from './Header'
-import {BrowserRouter} from "react-router-dom";
-import Home from './Homepage';
-import Landing from './Landing';
-//import Footer from './Footer';
->>>>>>> 5cac10e543fdeaf9c0e0cde7f62133c821c35615
 
+// const list = () => (<p> this is where a list component would go </p>);
+const index = () => (
+<main>
+  <Switch>
+    <Route exact path='/' component={Home}/>
+    <Route path='/todolist' component={Todolist}/>
+    <div> {List} </div>
+
+  </Switch>
+</main>
+)
+
+const Home = (props) => (
+<h2> To Do 2: The home component {console.log(props)} </h2> 
+)
+
+const List = () => (
+  <h2> insert todo list component here </h2> 
+)
+
+// const login = () => (
+// {Login}
+// )
 class App extends Component {
-
   render() {
     return (
-<<<<<<< HEAD
-      <div className="Page">
+      <div>
         <h1> To Do List: Get Your Shit Done! </h1>
         <nav className="navbar navbar-light">
           <ul className="nav navbar-nav">
@@ -46,20 +57,6 @@ class App extends Component {
       </Switch>
        </div>
     );
-=======
-      <div>
-        <div className = "container">
-          <Header />
-          <BrowserRouter>
-            <div>
-              <Route exact path="/" component={Home} /> 
-              <Route exact path="/dashboard" component={Landing} /> 
-            </div>
-          </BrowserRouter>
-        </div>
-      </div>
-    )
->>>>>>> 5cac10e543fdeaf9c0e0cde7f62133c821c35615
   }
 }
 
