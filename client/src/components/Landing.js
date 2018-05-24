2f4868a83b1d35036be9d6976a1c82bdb2bd8953
Anthony Cortez <anthony1154@Anthony-Cortezs-MacBook-Pro.local>
2018-05-23 20:14:03 -0700

    Updated with coneectewd database

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,6 +1,31 @@
 import React,  {Component} from 'react';
+import axios from 'axios';
 
 class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {tasks: null};
+  }
+
+  componentDidMount() {
+      axios.get('api/list').then(
+        result => {
+          // var array = result
+          // console.log(array)
+          // array.forEach(element => {
+          //   return <p> {element} </p>
+          // });
+          this.setState({tasks: [...result.data]}, ()=>{
+            console.log(this.state)
+          })
+        }
+      )
+    }
+
+    handleClick() {
+      console.log('click')
+    }
+
      render() {
        return (
          <div>
@@ -10,6 +35,11 @@ class Landing extends Component {
            <p>
              Below are your task.
            </p>  
+             { this.state.tasks &&
+                this.state.tasks.map(task => {
+                  return <p onClick={this.handleClick.bind(this)} key={task.id}>{task.id} {task.task}</p>
+                })
+             }
          </div>  
        )
      }
